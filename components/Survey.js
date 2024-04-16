b56b3a83919166a6c3a47ac34867dcecd3e15d71
Ahmad Ali <newTownElectroo@gmail.com>
2024-04-16 11:19:56 +0300

    added iframes to dynamic survyes

--- components/Survey.js
+++ components/Survey.js
@@ -2,7 +2,24 @@
 
 class Survey {
 
-    
+    steps = []
 
 }
 
+
+// fetch("./survies/loansSurvey.html", {
+//     mode: "no-cors",
+//     method: "post",
+//     headers: {
+//         'Content-Type': 'text/html',
+//         'Access-Control-Allow-Origin': '*'
+//     }
+// })
+// .then(response => {
+//     console.log(response.type)
+//     return response.text();
+// })
+// .then((d) => {
+//     document.body.innerHTML = d
+// })
+// .catch(error => console.error('Fetch error:', error))
